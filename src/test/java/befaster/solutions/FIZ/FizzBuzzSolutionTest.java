5a09d2ff13445631c5d433a7c5b8f7ffa4b04168
sbx_user1060 <sbx_user1060@ip-10-129-55-244.eu-west-2.compute.internal>
2018-09-08 15:55:54 +0000

    Sat Sep 08 15:55:54 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -2,23 +2,26 @@ package befaster.solutions.FIZ;
 
 import static org.junit.Assert.*;
 
+import java.util.stream.Stream;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class FizzBuzzSolutionTest {
-
+	private FizzBuzzSolution fzbzSolution;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
 
 	@Before
 	public void setUp() throws Exception {
+		fzbzSolution = new FizzBuzzSolution();
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertThat(Stream.iterate(1, (i)->i+1).limit(9999).map(FizzBuzzSolution::fizzBuzz).forEach(System.out::println);
 	}
 
 }
