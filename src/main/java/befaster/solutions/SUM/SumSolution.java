e45d4dd609ad0af81ec1c4ebde7bdf7669db8a4a
sbx_user1059 <sbx_user1059@ip-10-128-43-131.eu-west-2.compute.internal>
2018-09-08 13:26:36 +0000

    Sat Sep 08 13:26:36 UTC 2018

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -4,9 +4,9 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class SumSolution {
 
-    public int compute(int x, int y) throws Exception {
-    	/*if(x>100 || x<0) throw new Exception("Invalid Param[0], has to be between 0-100");
-    	if(y>100 || y<0) throw new Exception("Invalid Param[1], has to be between 0-100");*/
+    public int compute(int x, int y) {
+    	if(x>100 || x<0) throw new RuntimeException("Invalid Param[0], has to be between 0-100");
+    	if(y>100 || y<0) throw new RuntimeException("Invalid Param[1], has to be between 0-100");
         return x+y;
     }
 
