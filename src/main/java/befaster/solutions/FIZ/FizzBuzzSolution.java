ef89c4ca7145054169021b5f1ae18b5be8212620
sbx_user1060 <sbx_user1060@ip-10-129-55-244.eu-west-2.compute.internal>
2018-09-08 15:49:54 +0000

    Sat Sep 08 15:49:54 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,7 +5,16 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        throw new SolutionNotImplementedException();
+        String buzzWord = 1+"";
+    	
+        int isDivisibleByThree = number%3;
+        int isDibisibleByFive = number%5;
+        int isDivisibleByFifteen = number%15;
+        
+        if(isDivisibleByFifteen==0)return "fizz buzz";
+        if(isDivisibleByFive==0)return "buzz";
+        if(isDivisibleByThree==0)
+    	return "";
     }
 
 }
