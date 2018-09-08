5f78e53f833a9298d8717ce4d1cc65056129a821
sbx_user1060 <sbx_user1060@ip-10-129-55-244.eu-west-2.compute.internal>
2018-09-08 18:56:58 +0000

    Sat Sep 08 18:56:58 UTC 2018

---
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionR4Test.java
@@ -0,0 +1,55 @@
+package befaster.solutions.FIZ;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FizzBuzzSolutionR4Test {
+	private FizzBuzzSolution fz;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		fz = new FizzBuzzSolution();
+	}
+
+	@Test
+	public void test555() {
+		assertThat(fz.fizzBuzz(555), equalTo("fizz buzz fake deluxe"));
+	}
+	
+	@Test
+	public void test11() {
+		assertThat(fz.fizzBuzz(11), equalTo("deluxe"));
+	}
+	
+	@Test
+	public void test22() {
+		assertThat(fz.fizzBuzz(22), equalTo("deluxe"));
+	}
+	
+	@Test
+	public void test1111() {
+		assertThat(fz.fizzBuzz(1111), equalTo("deluxe"));
+	}
+
+	@Test
+	public void test811() {
+		assertThat(fz.fizzBuzz(811), equalTo("811"));
+	}
+	
+	@Test
+	public void test997() {
+		assertThat(fz.fizzBuzz(997), equalTo("997"));
+	}
+	
+	@Test
+	public void test27() {
+		assertThat(fz.fizzBuzz(27), equalTo("fizz"));
+	}
+}
